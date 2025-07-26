2f79ab88ea628e69eb1319c547dc664bb9f17bf4
LyokoJeremie <lucheng989898@gmail.com>
2025-07-27 00:15:32 +0800

    fix ModPack.ts

--- src/ModPack.ts
+++ src/ModPack.ts
@@ -93,9 +93,9 @@ function paddingToBlockSize(data: Uint8Array, blockSize: number): {
     paddedDataLength: number,
     filePath?: string,
 } {
-    const paddingLength = blockSize - (data.length % blockSize);
-    const blocks = Math.ceil(data.length / blockSize);
-    if (paddingLength === blockSize) {
+    let paddingLength = blockSize - (data.length % blockSize);
+    let blocks = Math.ceil(data.length / blockSize);
+    if (blocks > 0 && paddingLength === blockSize) {
         return {
             blocks: blocks,
             dataLength: data.length,
@@ -103,6 +103,11 @@ function paddingToBlockSize(data: Uint8Array, blockSize: number): {
             paddedData: data, // No padding needed
             paddedDataLength: data.length,
         }
+    }
+    if (blocks === 0) {
+        // If the data is empty, we still need at least one block
+        blocks = 1;
+        paddingLength = blockSize; // Full padding for the single block
     }
     if (paddingLength < 0 || paddingLength > blockSize) {
         throw new Error(`Invalid padding length: ${paddingLength}. Data length: ${data.length}, Block size: ${blockSize}`);
@@ -424,6 +429,13 @@ export class ModPackFileReader {
     static xxhashApi?: Awaited<ReturnType<typeof xxhash>>;
     private xxHashValue?: bigint;
 
+    get modPackBufferSize(): number {
+        if (!this.isInit) {
+            throw new Error('ModPackFileReader is not initialized. Please call load() first.');
+        }
+        return this.modPackBuffer.length;
+    }
+
     get modMetaInfo(): ModMeta {
         if (!this.isInit) {
             throw new Error('ModPackFileReader is not initialized. Please call load() first.');
@@ -508,6 +520,8 @@ export class ModPackFileReader {
             throw new Error('[ModPackFileReader] Invalid magic number in mod meta');
         }
 
+        this.modMeta = modMeta;
+
         // check ModMeta valid
         if (modMeta.protocolVersion !== ModMetaProtocolVersion) {
             console.error(`[ModPackFileReader] Invalid mod meta protocol version: ${modMeta.protocolVersion}, expected: ${ModMetaProtocolVersion}`);
@@ -522,7 +536,7 @@ export class ModPackFileReader {
             throw new Error(`[ModPackFileReader] Invalid boot json file meta: ${JSON.stringify(modMeta.bootJsonFile)}`);
         }
         for (const [filePath, fileMeta] of Object.entries(modMeta.fileMeta)) {
-            if (fileMeta.b < 0 || fileMeta.e < fileMeta.b || fileMeta.l <= 0) {
+            if (fileMeta.b < 0 || fileMeta.e < fileMeta.b || fileMeta.l < 0) {
                 console.error(`[ModPackFileReader] Invalid file meta for ${filePath}: ${JSON.stringify(fileMeta)}`);
                 throw new Error(`[ModPackFileReader] Invalid file meta for ${filePath}: ${JSON.stringify(fileMeta)}`);
             }
