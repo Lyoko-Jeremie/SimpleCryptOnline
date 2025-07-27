91baee466e62d8211fdc984edec9db407de2aa71
LyokoJeremie <lucheng989898@gmail.com>
2025-07-27 16:11:37 +0800

    ..

--- src/polyfill.ts
+++ src/polyfill.ts
@@ -1,1 +1,7 @@
 import 'core-js/full';
+import sodium from 'libsodium-wrappers-sumo';
+// @ts-ignore
+import xxhash from 'xxhash-wasm';
+
+(globalThis as any).sodium = sodium;
+(globalThis as any).xxhash = xxhash;
