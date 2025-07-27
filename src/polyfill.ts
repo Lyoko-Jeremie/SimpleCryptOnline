b28f8933f808c49b09dc34ead96c4fa8f0a8d177
LyokoJeremie <lucheng989898@gmail.com>
2025-07-27 16:13:51 +0800

    JSZip

--- src/polyfill.ts
+++ src/polyfill.ts
@@ -2,6 +2,8 @@ import 'core-js/full';
 import sodium from 'libsodium-wrappers-sumo';
 // @ts-ignore
 import xxhash from 'xxhash-wasm';
+import JSZip from 'jszip';
 
 (globalThis as any).sodium = sodium;
 (globalThis as any).xxhash = xxhash;
+(globalThis as any).JSZip = JSZip;
